657fa2232545f941429db647971ed72d0efc30cf
tatiana-passarella <tatiana.passarella@gmail.com>
2020-11-16 15:14:51 +0100

    Display contact avatar, name, email, and remove button

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -7,8 +7,20 @@ class ListContacts extends Component {
       <ol className='contact-list'>
 
         {this.props.contacts.map((contact) => (
-          <li key={contact.id}>
-            {contact.name}
+          <li key={contact.id} className='contact-list-item'>
+            <div
+              className='contact-avatar'
+              style={{
+                backgroundImage: `url(${contact.avatarURL})`
+              }}
+            ></div>
+            <div className='contact-details'>
+              <p>{contact.name}</p>
+              <p>{contact.handle}</p>
+            </div>
+            <button className='contact-remove'>
+              Remove
+            </button>
           </li>
         ))}
 
