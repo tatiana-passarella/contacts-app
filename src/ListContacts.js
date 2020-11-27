8571c0ffd0ed0b93498d77f4a9e4633bdfc5c133
tatiana-passarella <tatiana.passarella@gmail.com>
2020-11-27 16:49:38 +0100

    Adds 'Now showing' section with link to clear the search query

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-
 class ListContacts extends Component {
   static propTypes = {
     contacts: PropTypes.array.isRequired,
@@ -14,21 +13,38 @@ class ListContacts extends Component {
       query: query.trim()
     }))
   }
+  clearQuery = () => {
+    this.updateQuery('')
+  }
   render() {
+    const { query } = this.state
+    const { contacts, onDeleteContact } = this.props
+    const showingContacts = query === ''
+      ? contacts
+      : contacts.filter((c) => (
+          c.name.toLowerCase().includes(query.toLowerCase())
+        ))
     return (
       <div className='list-contacts'>
-        {JSON.stringify(this.state)}
         <div className='list-contacts-top'>
           <input
             className='search-contacts'
             type='text'
             placeholder='Search Contacts'
-            value={this.state.query}
+            value={query}
             onChange={(event) => this.updateQuery(event.target.value)}
           />
         </div>
+
+        {showingContacts.length !== contacts.length && (
+          <div className='showing-contacts'>
+            <span>Now showing {showingContacts.length} of {contacts.length}</span>
+            <button onClick={this.clearQuery}>Show all</button>
+          </div>
+        )}
+
         <ol className='contact-list'>
-          {this.props.contacts.map((contact) => (
+          {showingContacts.map((contact) => (
             <li key={contact.id} className='contact-list-item'>
               <div
                 className='contact-avatar'
@@ -41,7 +57,7 @@ class ListContacts extends Component {
                 <p>{contact.handle}</p>
               </div>
               <button
-                onClick={() => this.props.onDeleteContact(contact)}
+                onClick={() => onDeleteContact(contact)}
                 className='contact-remove'>
                   Remove
               </button>
@@ -50,8 +66,3 @@ class ListContacts extends Component {
         </ol>
       </div>
     )
-  }
-}
-
-
-export default ListContacts
